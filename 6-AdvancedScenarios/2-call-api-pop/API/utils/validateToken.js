b4154e79d11b2e9350af42fe4bdcb09508f4e4e4
derisen <dogan.erisen@gmail.com>
2021-05-12 16:44:16 -0700

    add comments

--- 6-AdvancedScenarios/2-call-api-pop/API/utils/validateToken.js
+++ 6-AdvancedScenarios/2-call-api-pop/API/utils/validateToken.js
@@ -8,17 +8,22 @@ const validatePoP = async (req, res, next) => {
     const decodedToken = jwtDecode(userToken, { complete: true });
     
     try {
+        // parse the JSON Web Key sent in cnf claim to get the public key 
         parsedToken = await parseJwk(decodedToken.payload.cnf.jwk, "RS256");
 
         try {
+            // verify the original token using public key
             verifiedToken = await jwtVerify(userToken, parsedToken);
             
             if (JSON.stringify(verifiedToken.payload) === JSON.stringify(decodedToken.payload)) {
                 
+                // check the method, path and origin of the verified PoP token
                 const checkMethod = verifiedToken.payload.m === req.method ? true : false;
                 const checkHost = verifiedToken.payload.u.includes(req.hostname) ? true : false;
                 const checkPath = verifiedToken.payload.p.includes(`${req.baseUrl}${req.path}`) ? true : false;
 
+                // if verified, attach the enveloped access token back to the request auth header
+                // as a bearer token, and let the passport-azure-ad verify the access token down the line
                 if (checkMethod && checkHost && checkPath) {
                     req.headers.authorization = "Bearer " + verifiedToken.payload.at;
                     return next();
