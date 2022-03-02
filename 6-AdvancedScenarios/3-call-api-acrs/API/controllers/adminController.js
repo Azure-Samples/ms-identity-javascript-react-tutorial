f19aa678e4034488da1060667e5ea7572334624c
Salman Salem (IFG) <v-ssalem@microsoft.com>
2022-03-02 07:30:05 -0800

    fixed display auth context name and handled PUT oprations in authContextGuard

--- 6-AdvancedScenarios/3-call-api-acrs/API/controllers/adminController.js
+++ 6-AdvancedScenarios/3-call-api-acrs/API/controllers/adminController.js
@@ -19,7 +19,7 @@ exports.postDetailsPage = async(req, res, next) => {
         const authContext = new AuthContext(
             req.session.account.idTokenClaims.tid, 
             req.body.authContext.split(' ')[0], // id
-            req.body.authContext.split(' ')[1], // displayName
+            req.body.authContext.replace(req.body.authContext.split(' ')[0],' '), // displayName
             req.body.operation
         );
 
