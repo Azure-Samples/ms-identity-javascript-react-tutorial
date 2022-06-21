ea56ddfbf97179909a3b78fa8a675f0eaa714141
Dogan Erisen <dogan.erisen@gmail.com>
2022-06-21 10:22:56 -0700

    add claims table

--- 3-Authorization-II/1-call-api/SPA/src/components/DataDisplay.jsx
+++ 3-Authorization-II/1-call-api/SPA/src/components/DataDisplay.jsx
@@ -1,5 +1,8 @@
-import "../styles/App.css";
+import { Table } from "react-bootstrap";
 import { protectedResources } from "../authConfig";
+import { createClaimsTable } from "../utils/claimUtils";
+
+import "../styles/App.css";
 
 export const ProfileData = (props) => {
     const tableRows = Object.entries(props.graphData).map((entry, index) => {
@@ -11,24 +14,24 @@ export const ProfileData = (props) => {
 
     return (
         <>
-        <div className="data-area-div">
-            <p>Calling <strong>Microsoft Graph API</strong>...</p>
-            <ul>
-                <li><strong>resource:</strong> <mark>User</mark> object</li>
-                <li><strong>endpoint:</strong> <mark>https://graph.microsoft.com/v1.0/me</mark></li>
-                <li><strong>scope:</strong> <mark>user.read</mark></li>
-            </ul>
-            <p>Contents of the <strong>response</strong> is below:</p>
-        </div>
-        <div className="data-area-div">
-            <table>
-                <thead>
-                </thead>
-                <tbody>
-                    {tableRows}
-                </tbody>
-            </table>
-        </div>
+            <div className="data-area-div">
+                <p>Calling <strong>Microsoft Graph API</strong>...</p>
+                <ul>
+                    <li><strong>resource:</strong> <mark>User</mark> object</li>
+                    <li><strong>endpoint:</strong> <mark>https://graph.microsoft.com/v1.0/me</mark></li>
+                    <li><strong>scope:</strong> <mark>user.read</mark></li>
+                </ul>
+                <p>Contents of the <strong>response</strong> is below:</p>
+            </div>
+            <div className="data-area-div">
+                <table>
+                    <thead>
+                    </thead>
+                    <tbody>
+                        {tableRows}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
@@ -43,23 +46,63 @@ export const HelloData = (props) => {
 
     return (
         <>
-        <div className="data-area-div">
-            <p>Calling <strong>custom protected web API</strong>...</p>
-            <ul>
-                <li><strong>endpoint:</strong> <mark>{protectedResources.apiHello.endpoint}</mark></li>
-                <li><strong>scope:</strong> <mark>{protectedResources.apiHello.scopes[0]}</mark></li>
-            </ul>
-            <p>Contents of the <strong>response</strong> is below:</p>
-        </div>
-        <div className="data-area-div">
-            <table>
-                <thead>
-                </thead>
-                <tbody>
-                    {tableRows}
-                </tbody>
-            </table>
-        </div>
+            <div className="data-area-div">
+                <p>Calling <strong>custom protected web API</strong>...</p>
+                <ul>
+                    <li><strong>endpoint:</strong> <mark>{protectedResources.apiHello.endpoint}</mark></li>
+                    <li><strong>scope:</strong> <mark>{protectedResources.apiHello.scopes[0]}</mark></li>
+                </ul>
+                <p>Contents of the <strong>response</strong> is below:</p>
+            </div>
+            <div className="data-area-div">
+                <table>
+                    <thead>
+                    </thead>
+                    <tbody>
+                        {tableRows}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
+
+export const IdTokenData = (props) => {
+    const tokenClaims = createClaimsTable(props.idTokenClaims);
+
+    const tableRow = Object.keys(tokenClaims).map((key, index) => {
+        return (
+            <tr key={key}>
+                {tokenClaims[key].map((claimItem) => (
+                    <td key={claimItem}>{claimItem}</td>
+                ))}
+            </tr>
+        );
+    });
+    return (
+        <>
+            <div className="data-area-div">
+                <p>
+                    See below the claims in your <strong> ID token </strong>. For more information, visit:{' '}
+                    <span>
+                        <a href="https://docs.microsoft.com/en-us/azure/active-directory/develop/id-tokens#claims-in-an-id-token">
+                        docs.microsoft.com
+                        </a>
+                    </span>
+                </p>
+                <div className="data-area-div">
+                    <Table responsive striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Claim</th>
+                                <th>Value</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>{tableRow}</tbody>
+                    </Table>
+                </div>
+            </div>
+        </>
+    );
+};
