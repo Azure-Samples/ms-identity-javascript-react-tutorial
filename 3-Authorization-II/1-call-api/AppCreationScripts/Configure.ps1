fbc3d07b97252b07343d28bbbbad0b38e3560aba
derisen <dogan.ersen@gmal.com>
2022-07-23 14:19:20 -0700

    add optional claim to sample.json

--- 3-Authorization-II/1-call-api/AppCreationScripts/Configure.ps1
+++ 3-Authorization-II/1-call-api/AppCreationScripts/Configure.ps1
@@ -12,12 +12,12 @@ param(
  for the visual Studio projects from the data in the Azure AD applications.
 
  In case you don't have Microsoft.Graph.Applications already installed, the script will automatically install it for the current user
-
+ 
  There are four ways to run this script. For more information, read the AppCreationScripts.md file in the same folder as this script.
 #>
 
 # Adds the requiredAccesses (expressed as a pipe separated string) to the requiredAccess structure
-# The exposed permissions are in the $exposedPermissions collection, and the type of permission (Scope | Role) is
+# The exposed permissions are in the $exposedPermissions collection, and the type of permission (Scope | Role) is 
 # described in $permissionType
 Function AddResourcePermission($requiredAccess, `
                                $exposedPermissions, [string]$requiredAccesses, [string]$permissionType)
@@ -53,7 +53,7 @@ Function GetRequiredPermissions([string] $applicationDisplayName, [string] $requ
     }
     $appid = $sp.AppId
     $requiredAccess = New-Object Microsoft.Graph.PowerShell.Models.MicrosoftGraphRequiredResourceAccess
-    $requiredAccess.ResourceAppId = $appid
+    $requiredAccess.ResourceAppId = $appid 
     $requiredAccess.ResourceAccess = New-Object System.Collections.Generic.List[Microsoft.Graph.PowerShell.Models.MicrosoftGraphResourceAccess]
 
     # $sp.Oauth2Permissions | Select Id,AdminConsentDisplayName,Value: To see the list of all the Delegated permissions for the application:
@@ -61,7 +61,7 @@ Function GetRequiredPermissions([string] $applicationDisplayName, [string] $requ
     {
         AddResourcePermission $requiredAccess -exposedPermissions $sp.Oauth2PermissionScopes -requiredAccesses $requiredDelegatedPermissions -permissionType "Scope"
     }
-
+    
     # $sp.AppRoles | Select Id,AdminConsentDisplayName,Value: To see the list of all the Application permissions for the application
     if ($requiredApplicationPermissions)
     {
@@ -103,7 +103,7 @@ Function ReplaceInTextFile([string] $configFilePath, [System.Collections.HashTab
 }
 <#.Description
    This function creates a new Azure AD scope (OAuth2Permission) with default and provided values
-#>
+#>  
 Function CreateScope( [string] $value, [string] $userConsentDisplayName, [string] $userConsentDescription, [string] $adminConsentDisplayName, [string] $adminConsentDescription)
 {
     $scope = New-Object Microsoft.Graph.PowerShell.Models.MicrosoftGraphPermissionScope
@@ -120,7 +120,7 @@ Function CreateScope( [string] $value, [string] $userConsentDisplayName, [string
 
 <#.Description
    This function creates a new Azure AD AppRole with default and provided values
-#>
+#>  
 Function CreateAppRole([string] $types, [string] $name, [string] $description)
 {
     $appRole = New-Object Microsoft.Graph.PowerShell.Models.MicrosoftGraphAppRole
@@ -137,6 +137,19 @@ Function CreateAppRole([string] $types, [string] $name, [string] $description)
     $appRole.Value = $name;
     return $appRole
 }
+Function CreateOptionalClaim([string] $name)
+{
+    <#.Description
+    This function creates a new Azure AD optional claims  with default and provided values
+    #>  
+
+    $appClaim = New-Object Microsoft.Graph.PowerShell.Models.MicrosoftGraphOptionalClaim
+    $appClaim.AdditionalProperties =  New-Object System.Collections.Generic.List[string]
+    $appClaim.Source =  $null
+    $appClaim.Essential = $false
+    $appClaim.Name = $name
+    return $appClaim
+}
 
 
 Function ConfigureApplications
@@ -145,8 +158,8 @@ Function ConfigureApplications
        This function creates the Azure AD applications for the sample in the provided Azure AD tenant and updates the
        configuration files in the client and service project  of the visual studio solution (App.Config and Web.Config)
        so that they are consistent with the Applications parameters
-    #>
-
+    #> 
+    
     if (!$azureEnvironmentName)
     {
         $azureEnvironmentName = "Global"
@@ -161,13 +174,16 @@ Function ConfigureApplications
     else {
         Connect-MgGraph -TenantId $tenantId -Scopes "Application.ReadWrite.All" -Environment $azureEnvironmentName
     }
-
+    
 
    # Create the service AAD application
    Write-Host "Creating the AAD application (msal-node-api)"
-
-   # create the application
+   
+   # create the application 
    $serviceAadApplication = New-MgApplication -DisplayName "msal-node-api" `
+                                                       -Web `
+                                                       @{ `
+                                                         } `
                                                          -Api `
                                                          @{ `
                                                             RequestedAccessTokenVersion = 2 `
@@ -176,36 +192,50 @@ Function ConfigureApplications
                                                        #end of command
     $serviceIdentifierUri = 'api://'+$serviceAadApplication.AppId
     Update-MgApplication -ApplicationId $serviceAadApplication.Id -IdentifierUris @($serviceIdentifierUri)
-
-    # create the service principal of the newly created application
+    
+    # create the service principal of the newly created application 
     $currentAppId = $serviceAadApplication.AppId
     $serviceServicePrincipal = New-MgServicePrincipal -AppId $currentAppId -Tags {WindowsAzureActiveDirectoryIntegratedApp}
 
     # add the user running the script as an app owner if needed
     $owner = Get-MgApplicationOwner -ApplicationId $serviceAadApplication.Id
     if ($owner -eq $null)
-    {
+    { 
         New-MgApplicationOwnerByRef -ApplicationId $serviceAadApplication.Id  -BodyParameter = @{"@odata.id" = "htps://graph.microsoft.com/v1.0/directoryObjects/$user.ObjectId"}
         Write-Host "'$($user.UserPrincipalName)' added as an application owner to app '$($serviceServicePrincipal.DisplayName)'"
     }
 
+    # Add Claims
+
+    $optionalClaims = New-Object Microsoft.Graph.PowerShell.Models.MicrosoftGraphOptionalClaims
+    $optionalClaims.AccessToken = New-Object System.Collections.Generic.List[Microsoft.Graph.PowerShell.Models.MicrosoftGraphOptionalClaim]
+    $optionalClaims.IdToken = New-Object System.Collections.Generic.List[Microsoft.Graph.PowerShell.Models.MicrosoftGraphOptionalClaim]
+    $optionalClaims.Saml2Token = New-Object System.Collections.Generic.List[Microsoft.Graph.PowerShell.Models.MicrosoftGraphOptionalClaim]
+
+
+    # Add Optional Claims
+
+    $newClaim =  CreateOptionalClaim  -name "idtyp" 
+    $optionalClaims.AccessToken += ($newClaim)
+    Update-MgApplication -ApplicationId $serviceAadApplication.Id -OptionalClaims $optionalClaims
+    
     # Add application Roles
     $appRoles = New-Object System.Collections.Generic.List[Microsoft.Graph.PowerShell.Models.MicrosoftGraphAppRole]
-    $newRole = CreateAppRole -types "Application" -name "Todolist.Read.All" -description "Application can only read Todo list"
+    $newRole = CreateAppRole -types "Application" -name "Todolist.Read.All" -description "Allow this application to read every users Todo list items"
     $appRoles.Add($newRole)
-    $newRole = CreateAppRole -types "Application" -name "Todolist.ReadWrite.All" -description "Application can read and write into Todo list"
+    $newRole = CreateAppRole -types "Application" -name "Todolist.ReadWrite.All" -description "Allow this application to read and write every users Todo list items"
     $appRoles.Add($newRole)
     Update-MgApplication -ApplicationId $serviceAadApplication.Id -AppRoles $appRoles
-
+    
     # rename the user_impersonation scope if it exists to match the readme steps or add a new scope
-
+       
     # delete default scope i.e. User_impersonation
     # Alex: the scope deletion doesn't work - see open issue - https://github.com/microsoftgraph/msgraph-sdk-powershell/issues/1054
     $scopes = New-Object System.Collections.Generic.List[Microsoft.Graph.PowerShell.Models.MicrosoftGraphPermissionScope]
     $scope = $serviceAadApplication.Api.Oauth2PermissionScopes | Where-Object { $_.Value -eq "User_impersonation" }
-
+    
     if($scope -ne $null)
-    {
+    {    
         # disable the scope
         $scope.IsEnabled = $false
         $scopes.Add($scope)
@@ -221,16 +251,16 @@ Function ConfigureApplications
     -userConsentDescription "Allow the application to access msal-node-api on your behalf."  `
     -adminConsentDisplayName "Access msal-node-api"  `
     -adminConsentDescription "Allows the app to have the same access to information in the directory on behalf of the signed-in user."
-
+            
     $scopes.Add($scope)
     $scope = CreateScope -value Todolist.ReadWrite  `
     -userConsentDisplayName "Access msal-node-api"  `
     -userConsentDescription "Allow the application to access msal-node-api on your behalf."  `
     -adminConsentDisplayName "Access msal-node-api"  `
     -adminConsentDescription "Allows the app to have the same access to information in the directory on behalf of the signed-in user."
-
+            
     $scopes.Add($scope)
-
+    
     # add/update scopes
     Update-MgApplication -ApplicationId $serviceAadApplication.Id -Api @{Oauth2PermissionScopes = @($scopes)}
     Write-Host "Done creating the service application (msal-node-api)"
@@ -242,26 +272,26 @@ Function ConfigureApplications
 
    # Create the client AAD application
    Write-Host "Creating the AAD application (msal-react-spa)"
-
-   # create the application
+   
+   # create the application 
    $clientAadApplication = New-MgApplication -DisplayName "msal-react-spa" `
                                                       -Spa `
                                                       @{ `
-                                                          RedirectUris = "http://localhost:3000", "http://localhost:3000/redirect.html"; `
+                                                          RedirectUris = "https://localhost:3000", "http://localhost:3000/redirect.html"; `
                                                         } `
                                                        -SignInAudience AzureADMyOrg `
                                                       #end of command
     $tenantName = (Get-MgApplication -ApplicationId $clientAadApplication.Id).PublisherDomain
     Update-MgApplication -ApplicationId $clientAadApplication.Id -IdentifierUris @("https://$tenantName/msal-react-spa")
-
-    # create the service principal of the newly created application
+    
+    # create the service principal of the newly created application 
     $currentAppId = $clientAadApplication.AppId
     $clientServicePrincipal = New-MgServicePrincipal -AppId $currentAppId -Tags {WindowsAzureActiveDirectoryIntegratedApp}
 
     # add the user running the script as an app owner if needed
     $owner = Get-MgApplicationOwner -ApplicationId $clientAadApplication.Id
     if ($owner -eq $null)
-    {
+    { 
         New-MgApplicationOwnerByRef -ApplicationId $clientAadApplication.Id  -BodyParameter = @{"@odata.id" = "htps://graph.microsoft.com/v1.0/directoryObjects/$user.ObjectId"}
         Write-Host "'$($user.UserPrincipalName)' added as an application owner to app '$($clientServicePrincipal.DisplayName)'"
     }
@@ -273,17 +303,17 @@ Function ConfigureApplications
     Add-Content -Value "<tr><td>client</td><td>$currentAppId</td><td><a href='$clientPortalUrl'>msal-react-spa</a></td></tr>" -Path createdApps.html
     $requiredResourcesAccess = New-Object System.Collections.Generic.List[Microsoft.Graph.PowerShell.Models.MicrosoftGraphRequiredResourceAccess]
 
-
+    
     # Add Required Resources Access (from 'client' to 'service')
     Write-Host "Getting access from 'client' to 'service'"
     $requiredPermissions = GetRequiredPermissions -applicationDisplayName "msal-node-api" `
         -requiredDelegatedPermissions "Todolist.Read|Todolist.ReadWrite" `
-
+    
 
     $requiredResourcesAccess.Add($requiredPermissions)
     Update-MgApplication -ApplicationId $clientAadApplication.Id -RequiredResourceAccess $requiredResourcesAccess
     Write-Host "Granted permissions."
-
+    
     # Update config file for 'service'
     $configFile = $pwd.Path + "\..\API\authConfig.js"
     $dictionary = @{ "Enter_the_Application_Id_Here" = $serviceAadApplication.AppId;"Enter_the_Tenant_Info_Here" = $tenantId };
@@ -291,7 +321,7 @@ Function ConfigureApplications
     Write-Host "Updating the sample code ($configFile)"
 
     ReplaceInTextFile -configFilePath $configFile -dictionary $dictionary
-
+    
     # Update config file for 'client'
     $configFile = $pwd.Path + "\..\SPA\src\authConfig.js"
     $dictionary = @{ "Enter_the_Application_Id_Here" = $clientAadApplication.AppId;"Enter_the_Tenant_Info_Here" = $tenantId;"Enter_the_Web_Api_Application_Id_Here" = $serviceAadApplication.AppId };
@@ -301,17 +331,17 @@ Function ConfigureApplications
     ReplaceInTextFile -configFilePath $configFile -dictionary $dictionary
     if($isOpenSSL -eq 'Y')
     {
-        Write-Host -ForegroundColor Green "------------------------------------------------------------------------------------------------"
+        Write-Host -ForegroundColor Green "------------------------------------------------------------------------------------------------" 
         Write-Host "You have generated certificate using OpenSSL so follow below steps: "
         Write-Host "Install the certificate on your system from current folder."
-        Write-Host -ForegroundColor Green "------------------------------------------------------------------------------------------------"
+        Write-Host -ForegroundColor Green "------------------------------------------------------------------------------------------------" 
     }
-    Add-Content -Value "</tbody></table></body></html>" -Path createdApps.html
+    Add-Content -Value "</tbody></table></body></html>" -Path createdApps.html  
 }
 
 # Pre-requisites
 if ($null -eq (Get-Module -ListAvailable -Name "Microsoft.Graph.Applications")) {
-    Install-Module "Microsoft.Graph.Applications" -Scope CurrentUser
+    Install-Module "Microsoft.Graph.Applications" -Scope CurrentUser 
 }
 
 Import-Module Microsoft.Graph.Applications
