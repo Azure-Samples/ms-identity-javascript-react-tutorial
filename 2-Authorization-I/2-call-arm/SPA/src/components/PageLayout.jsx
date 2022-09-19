da9d0b32be25b2f614ec3c0b9901fcdee295f84d
salman90 <v-ssalem@microsoft.com>
2022-09-19 16:57:09 -0700

    minro updates

--- 2-Authorization-I/2-call-arm/SPA/src/components/PageLayout.jsx
+++ 2-Authorization-I/2-call-arm/SPA/src/components/PageLayout.jsx
@@ -24,6 +24,7 @@ export const PageLayout = (props) => {
                         <a
                             href="https://forms.office.com/Pages/ResponsePage.aspx?id=v4j5cvGGr0GRqy180BHbR73pcsbpbxNJuZCMKN0lURpUMlRHSkc5U1NLUkxFNEtVN0dEOTFNQkdTWiQlQCN0PWcu"
                             target="_blank"
+                            rel="noopener noreferrer"
                         >
                             {' '}
                             Share your experience!
