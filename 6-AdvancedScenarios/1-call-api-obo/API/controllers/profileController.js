92571b87a254272ec1718d7ab6515a49644b1b53
salman90 <v-ssalem@microsoft.com>
2022-12-21 16:19:31 -0800

    remove app permission from sample

--- 6-AdvancedScenarios/1-call-api-obo/API/controllers/profileController.js
+++ 6-AdvancedScenarios/1-call-api-obo/API/controllers/profileController.js
@@ -1,56 +1,27 @@
 const { getOboToken } = require('../MsalOnBehalfOfClient');
 const { getGraphClient } = require('../util/graphClient');
 const { ResponseType } = require('@microsoft/microsoft-graph-client');
-const  authConfig  = require('../authConfig');
+const authConfig = require('../authConfig');
 
 const {
-    isAppOnlyToken,
     hasRequiredDelegatedPermissions,
-    hasRequiredApplicationPermissions,
 } = require('../auth/permissionUtils');
 
-
 exports.getProfile = async (req, res, next) => {
-    if (isAppOnlyToken(req.authInfo)) {
-        if (
-            hasRequiredApplicationPermissions(
-                req.authInfo,
-                authConfig.resources.middleTierAPI.applicationPermissions.scopes
-            )
-        ) {
-            try {                
-                 accessToken = await getOboToken(tokenValue);
-                 let graphResponse = await getGraphClient(accessToken).api('/me').responseType(ResponseType.RAW).get();
-                 graphResponse = await graphResponse.json();
-                 res.status(200).send(graphResponse);
-             } catch (error) {
-                 next(error);
-             }
-        }else {
-             next(new Error('Application does not have the required permissions'));
+    const userToken = req.get('authorization');
+    const [bearer, tokenValue] = userToken.split(' ');
+
+    let accessToken;
+    if (hasRequiredDelegatedPermissions(req.authInfo, authConfig.resources.middleTierAPI.delegatedPermissions.scopes)) {
+        try {
+            accessToken = await getOboToken(tokenValue);
+            let graphResponse = await getGraphClient(accessToken).api('/me').responseType(ResponseType.RAW).get();
+            graphResponse = await graphResponse.json();
+            res.json(graphResponse);
+        } catch (error) {
+            next(error);
         }
     } else {
-        const userToken = req.get('authorization');
-        const [bearer, tokenValue] = userToken.split(' ');
-        
-        let accessToken;
-        if (
-            hasRequiredDelegatedPermissions(
-                req.authInfo,
-                authConfig.resources.middleTierAPI.delegatedPermissions.scopes
-            )
-        ) {
-            try {
-                accessToken = await getOboToken(tokenValue);
-                let graphResponse = await getGraphClient(accessToken).api('/me').responseType(ResponseType.RAW).get();
-                graphResponse = await graphResponse.json();
-                res.json(graphResponse);
-            } catch (error) {
-                next(error);
-            }
-        } else {
-            next(new Error('User does not have the required permissions'));
-        }
-        
-    } 
+        next(new Error('User does not have the required permissions'));
+    }
 };
