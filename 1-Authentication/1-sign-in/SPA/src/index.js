280a5e2e0ecfbc3c23d6d191766dc737cd6d7567
Daniel Steinberg <daniel.steinberg@accenture.com>
2023-12-14 15:08:35 -0500

    Found error in active account initialization in the 1-sign-in tutorial

--- 1-Authentication/1-sign-in/SPA/src/index.js
+++ 1-Authentication/1-sign-in/SPA/src/index.js
@@ -16,7 +16,7 @@ const msalInstance = new PublicClientApplication(msalConfig);
 // Default to using the first account if no account is active on page load
 if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
     // Account selection logic is app dependent. Adjust as needed for different use cases.
-    msalInstance.setActiveAccount(msalInstance.getActiveAccount()[0]);
+    msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
 }
 
 // Listen for sign-in event and set active account
